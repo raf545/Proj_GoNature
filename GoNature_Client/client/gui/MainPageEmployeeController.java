81c1e6d4e7554177484b18341f7dc924c792546b
yansokolov <yansokolov@10.0.0.21>
2020-12-18 11:34:08 +0200

    add employee

--- GoNature_Client/client/gui/MainPageEmployeeController.java
+++ GoNature_Client/client/gui/MainPageEmployeeController.java
@@ -1,5 +1,13 @@
 package gui;
 
+import logic.Employee;
+
 public class MainPageEmployeeController {
+
+	Employee employee;
+
+	void setEmployee(Employee employeeFromDB) {
+		employee = employeeFromDB;
+	}
 
 }
