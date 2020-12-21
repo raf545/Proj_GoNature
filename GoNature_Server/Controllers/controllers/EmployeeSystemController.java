e35619cc847abcf2d81bd88dc7bbd86c7be12460
yansokolov <yansokolov@LAPTOP-50KQQJU8>
2020-12-21 20:55:09 +0200

    set family subscruber

--- GoNature_Server/Controllers/controllers/EmployeeSystemController.java
+++ GoNature_Server/Controllers/controllers/EmployeeSystemController.java
@@ -32,7 +32,7 @@ public class EmployeeSystemController {
 		switch (MethodName) {
 		case "addFamilySub":
 			return addFamilySub(data, client);
-			
+
 		case "addInstructorSub":
 			return addInstructorSub(data, client);
 
@@ -48,29 +48,34 @@ public class EmployeeSystemController {
 		ResultSet res = SqlConnector.getInstance().searchInDB(query);
 		if (isEmpty(res) != 0)
 			return "subscriber already exist";
-		query="SELECT * FROM gonaturedb.uptodateinformation WHERE nameOfVal = subscriberID;";
-		res=SqlConnector.getInstance().searchInDB(query);
+		query = "SELECT * FROM gonaturedb.uptodateinformation WHERE nameOfVal = \"subscriberID\";";
+		res = SqlConnector.getInstance().searchInDB(query);
+		if (isEmpty(res) == 0) {
+			System.out.println("here!!");
+			return "";
+		}
 		try {
-			subidr=res.getInt("value");
+			System.out.println(res.getString(1) + "!!!!!!");
+			subidr = res.getInt(2);
 			subscriber.setSubscriberId(String.valueOf(subidr));
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		query = "INSERT INTO gonaturedb.subscriber (id, subscriberid, name, lastName, phone, email, numOfMembers, creditCardNumber, subscriberTypre) VALUES "
 				+ subscriber.toString() + ";";
 		if (SqlConnector.getInstance().updateToDB(query)) {
 			subidr++;
-			query="UPDATE gonaturedb.uptodateinformation SET value = "+subidr+" WHERE (nameOfVal = subscriberID);";
-			return gson.toJson(subscriber);
+			query = "UPDATE gonaturedb.uptodateinformation SET num = " + subidr + " WHERE (nameOfVal = \"subscriberID\");";
+			if (SqlConnector.getInstance().updateToDB(query)) {
+				return gson.toJson(subscriber);
+			}
 		}
-			
+
 		return "fail";
 	}
-	
-	
-	
+
 	private String addInstructorSub(String data, ConnectionToClient client) {
 		Subscriber subscriber = gson.fromJson(data, Subscriber.class);
 		String query = "SELECT * FROM gonaturedb.subscriber WHERE id = " + subscriber.getId() + " OR subscriberid = "
@@ -78,19 +83,13 @@ public class EmployeeSystemController {
 		ResultSet res = SqlConnector.getInstance().searchInDB(query);
 		if (isEmpty(res) != 0)
 			return "Instructor already exist";
-	
-			
+
 		query = "INSERT INTO gonaturedb.subscriber (id, subscriberid, name, lastName, phone, email, numOfMembers, creditCardNumber, subscriberTypre) VALUES "
 				+ subscriber.toString() + ";";
 		if (SqlConnector.getInstance().updateToDB(query))
 			return "success";
 		return "fail";
 	}
-
-	
-	
-	
-	
 
 	private int isEmpty(ResultSet rs) {
 		int size = 0;
