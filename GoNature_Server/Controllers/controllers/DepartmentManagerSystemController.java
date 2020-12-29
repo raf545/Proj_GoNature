b74a9698e4ddab0e1d11048afe3eb51895845190
שי מריומה < @LAPTOP-4R6HQGU4>
2020-12-29 23:11:24 +0200

    reports for department manager

--- GoNature_Server/Controllers/controllers/DepartmentManagerSystemController.java
+++ GoNature_Server/Controllers/controllers/DepartmentManagerSystemController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import com.google.gson.Gson;
 
@@ -37,11 +38,78 @@ public class DepartmentManagerSystemController {
 			return updateFieldStatus(values[1],values[0]);
 		case "updateParkInformation":
 			String[] values2 = data.split(" ");
-			return updateParkInformation(values2[0],values2[1],values2[2]);
+			return updateParkInformation(values2[0],values2[1],values2[2]);		
+		case "getEntryDetailsByHours":
+			String[] values3 = data.split(" ");
+			return getEntryDetailsByHours(values3[0],values3[1],values3[2],values3[3]);
+		case "getExitDetailsByHours":
+			String[] values4 = data.split(" ");
+			return getExitDetailsByHours(values4[0],values4[1],values4[2],values4[3]);
 		}
 		return data;
 	}
-
+	
+	//Functions for visitor reports
+	private String getEntryDetailsByHours(String type,String year,String month,String day)
+	{
+		try {
+		String query;
+		StringBuilder sb = new StringBuilder();
+		ResultSet rs;
+		
+		query = "SELECT HOUR(gonaturedb.cardreader.entryTime) as hoursEntry, SUM(numberOfVisitors) as counter\r\n"
+				+ "FROM gonaturedb.cardreader\r\n"
+				+ "WHERE typeOfVisitor = \"" + type + "\" AND YEAR(entryTime) = " + year + " AND MONTH(entryTime) = " + month + " AND DAY(entryTime) = " + day +"\r\n"
+				+ "GROUP BY HOUR(entryTime)\r\n"
+				+ "ORDER BY hoursEntry ASC";
+		
+			rs = DataBase.getInstance().search(query);
+			if (isEmpty(rs) != 0) 
+			{
+				rs.beforeFirst();
+				while(rs.next())
+				{
+					sb.append(rs.getInt(1) + "," + rs.getInt(2) + " ");				
+				}
+			}
+		return sb.toString();
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		return "faild";	
+	}
+	//Functions for visitor reports
+	
+	private String getExitDetailsByHours(String type,String year,String month,String day)
+	{
+		try {
+		String query;
+		StringBuilder sb = new StringBuilder();
+		ResultSet rs;
+		
+		query = "SELECT HOUR(gonaturedb.cardreader.exitTime) as hoursExit, SUM(numberOfVisitors) as counter\r\n"
+				+ "FROM gonaturedb.cardreader\r\n"
+				+ "WHERE typeOfVisitor = \"" + type + "\" AND YEAR(exitTime) = " + year + " AND MONTH(exitTime) = " + month + " AND DAY(exitTime) = " + day +"\r\n"
+				+ "GROUP BY HOUR(exitTime)\r\n"
+				+ "ORDER BY hoursExit ASC";
+		
+			rs = DataBase.getInstance().search(query);
+			if (isEmpty(rs) != 0) 
+			{
+				rs.beforeFirst();
+				while(rs.next())
+				{
+					sb.append(rs.getInt(1) + "," + rs.getInt(2) + " ");				
+				}
+			}
+		return sb.toString();
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		return "faild";	
+	}
 	private String showAllApprovesAndRejects() {
 		
 		try {
