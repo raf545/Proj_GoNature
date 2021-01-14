7b9e2e202a7da7ccfe777e6b917e50eddc7f3254
yansokolov <yansokolov@LAPTOP-50KQQJU8>
2021-01-14 16:37:17 +0200

    Refael tipesh

--- GoNature_Server/unitests/departmentManagerSystemController/DepartmentManagerSystemControllerTest.java
+++ GoNature_Server/unitests/departmentManagerSystemController/DepartmentManagerSystemControllerTest.java
@@ -18,12 +18,13 @@ import dataBase.DataBase;
 
 /**
  * Test the department manager visitors report.
+ * 
  * @author Shay Maryuma
  * @author Ziv Tziyonit
  *
  */
 class DepartmentManagerSystemControllerTest {
-	private int i=0;
+	private int i = 0;
 	@Mock
 	Connection con;
 	@Mock
@@ -31,14 +32,16 @@ class DepartmentManagerSystemControllerTest {
 	@Mock
 	PreparedStatement quer;
 	@Mock
-	 DataBase db;
+	DataBase db;
 	@Mock
 	ResultSet rs;
-	
+
 	DepartmentManagerSystemController cont;
-	
+
 	/**
-	 * Create mocks for connection, prepareStatement, database, result set and department manager system controller.
+	 * Create mocks for connection, prepareStatement, database, result set and
+	 * department manager system controller.
+	 * 
 	 * @throws Exception
 	 */
 	@BeforeEach
@@ -46,88 +49,105 @@ class DepartmentManagerSystemControllerTest {
 
 		con = mock(Connection.class);
 		pre = mock(PreparedStatement.class);
-		db= mock(DataBase.class);
-		rs=mock(ResultSet.class);
-		cont=DepartmentManagerSystemController.getInstance();		
+		db = mock(DataBase.class);
+		rs = mock(ResultSet.class);
+		cont = DepartmentManagerSystemController.getInstance();
 	}
-	
+
 	/**
-	 * Check if the entry function in department manager's class is working properly.
-	 * arr1 is the array that holds hours of entering the park.
-	 * arr2 is the array that holds the amount of visitors in the specific hour.
-	 * index i will be the size of the table of the result set.
-	 * expected output: "12,6 14,5 "
+	 * Check if the entry function in department manager's class is working
+	 * properly. arr1 is the array that holds hours of entering the park. arr2 is
+	 * the array that holds the amount of visitors in the specific hour. index i
+	 * will be the size of the table of the result set. expected output: "12,6 14,5
+	 * "
+	 * 
 	 * @throws SQLException
 	 */
 	@Test
 	void testEntery() throws SQLException {
-		i=0;
-		int[] arr1 = new int[] {12,14};
-		int[] arr2 = new int[] {6,5};
-		
+		i = 0;
+		int[] arr1 = new int[] { 12, 14 };
+		int[] arr2 = new int[] { 6, 5 };
+
 		String expected = "12,6 14,5 ";
 		DataBase.setInstance(db);
 		when(DataBase.getInstance().getConnection()).thenReturn(con);
 		when(con.prepareStatement(anyString())).thenReturn(pre);
-		when(db.search(pre)).thenReturn(rs);		
-		when(rs.next()).thenAnswer(new Answer<Boolean>() {
-			
-			//We will override the function in order to achieve an iterator the size of the expected answer.
-			@Override
-			public Boolean answer(InvocationOnMock invocation) throws Throwable {		
-				if (i<2)
-				{
-					i++;
-					return true;
-				}
-				return false;
-			}			
-		});
-		when(rs.getRow()).thenReturn(2);
-		when (rs.getInt(1)).thenAnswer((a)->{return arr1[0];}).thenAnswer((a)->{return arr1[1];});
-		when (rs.getInt(2)).thenAnswer((a)->{return arr2[0];}).thenAnswer((a)->{return arr2[1];});
-		assertEquals(expected, cont.getFunc("getEntryDetailsByHours","x 1 1 1 x x x",null));;
-	}
-	
-	/**
-	 * Check if the exit function in department manager's class is working properly.
-	 * arr1 is the array that holds hours of exiting the park.
-	 * arr2 is the array that holds the amount of visitors in the specific hour.
-	 * index i will be the size of the table of the result set.
-	 * expected output: "17,8 12,3 "
-	 * @throws SQLException
-	 */ 
-	@Test
-	void testExit() throws SQLException {
-		i=0;
-		int[] arr1 = new int[] {17,12};
-		int[] arr2 = new int[] {8,3};
-		
-		String expected = "17,8 12,3 ";
-		DataBase.setInstance(db);
-		when(DataBase.getInstance().getConnection()).thenReturn(con);
-		when(con.prepareStatement(anyString())).thenReturn(pre);
-		when(db.search(pre)).thenReturn(rs);	
+		when(db.search(pre)).thenReturn(rs);
 		when(rs.next()).thenAnswer(new Answer<Boolean>() {
 
+			// We will override the function in order to achieve an iterator the size of the
+			// expected answer.
 			@Override
 			public Boolean answer(InvocationOnMock invocation) throws Throwable {
-			
-				if (i<2)
-				{
+				if (i < 2) {
 					i++;
 					return true;
 				}
 				return false;
 			}
-			
 		});
 		when(rs.getRow()).thenReturn(2);
-		when (rs.getInt(1)).thenAnswer((a)->{return arr1[0];}).thenAnswer((a)->{return arr1[1];});
-		when (rs.getInt(2)).thenAnswer((a)->{return arr2[0];}).thenAnswer((a)->{return arr2[1];});
-		assertEquals(expected, cont.getFunc("getExitDetailsByHours","x 1 1 1 x x x",null));;
-	
+		when(rs.getInt(1)).thenAnswer((a) -> {
+			return arr1[0];
+		}).thenAnswer((a) -> {
+			return arr1[1];
+		});
+		when(rs.getInt(2)).thenAnswer((a) -> {
+			return arr2[0];
+		}).thenAnswer((a) -> {
+			return arr2[1];
+		});
+		assertEquals(expected, cont.getFunc("getEntryDetailsByHours", "x 1 1 1 x x x", null));
+		;
 	}
-	
+
+	/**
+	 * Check if the exit function in department manager's class is working properly.
+	 * arr1 is the array that holds hours of exiting the park. arr2 is the array
+	 * that holds the amount of visitors in the specific hour. index i will be the
+	 * size of the table of the result set. expected output: "17,8 12,3 "
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	void testExit() throws SQLException {
+		i = 0;
+		int[] expectedHours = new int[] { 17, 12 };
+		int[] expectedAmounts = new int[] { 8, 3 };
+
+		String expected = "17,8 12,3 ";
+		DataBase.setInstance(db);
+		when(DataBase.getInstance().getConnection()).thenReturn(con);
+		when(con.prepareStatement(anyString())).thenReturn(pre);
+		when(db.search(pre)).thenReturn(rs);
+		when(rs.next()).thenAnswer(new Answer<Boolean>() {
+
+			@Override
+			public Boolean answer(InvocationOnMock invocation) throws Throwable {
+
+				if (i < expectedHours.length) {
+					i++;
+					return true;
+				}
+				return false;
+			}
+
+		});
+		when(rs.getRow()).thenReturn(2);
+		when(rs.getInt(1)).thenAnswer((a) -> {
+			return expectedHours[0];
+		}).thenAnswer((a) -> {
+			return expectedHours[1];
+		});
+		when(rs.getInt(2)).thenAnswer((a) -> {
+			return expectedAmounts[0];
+		}).thenAnswer((a) -> {
+			return expectedAmounts[1];
+		});
+		assertEquals(expected, cont.getFunc("getExitDetailsByHours", "x 1 1 1 x x x", null));
+		;
+
+	}
 
 }
